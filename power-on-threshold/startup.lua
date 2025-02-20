a7e4b4ed58580bdc498086c7130d43abba191b4a
xtul <mpiasecki720@protonmail.com>
2025-02-20 16:13:57 +0100

    fix code messing up in certain cases

--- power-on-threshold/startup.lua
+++ power-on-threshold/startup.lua
@@ -1,32 +1,37 @@
 local container = peripheral.find("inventory")
 
-if container == nil then
+if not container then
     print("No container found; please connect a container to the computer")
     return
 end
 
-local function isContainerFull()
-    for slot, _ in pairs(container.list()) do
-        local details = container.getItemDetail(slot)
-        local count = details.count
-        local maxCount = details.maxCount
+local redstoneSide = "back"
 
-        if count >= maxCount then
-            return true
+local function countFullStacks()
+    local fullStacks = 0
+
+    for slot, _ in ipairs(container.list()) do
+        local item = container.getItemDetail(slot)
+
+        if item.count >= item.maxCount then
+            fullStacks = fullStacks + 1
         end
     end
 
-    return false
+    return fullStacks
 end
 
-local redstoneSide = "back"
-
 while true do
-    if isContainerFull() then
+    local fullStacks = countFullStacks()
+
+    if fullStacks > 0 then
         redstone.setOutput(redstoneSide, false)
     else
+        -- turn on redstone only for a single tick
         redstone.setOutput(redstoneSide, true)
+        sleep(0.05)
+        redstone.setOutput(redstoneSide, false)
     end
 
-    sleep(0.5)
+    sleep(0.05)
 end
